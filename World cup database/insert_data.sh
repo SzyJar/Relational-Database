ca5dd896ce6a4f8600d5fff9f0bc209091a423a6
SzyJar <107247457+SzyJar@users.noreply.github.com>
2023-05-17 00:39:35 +0200

    World cup database - done

--- World cup database/insert_data.sh
+++ World cup database/insert_data.sh
@@ -10,7 +10,10 @@ fi
 # Do not change code above this line. Use the PSQL variable above to query your database.
 
 skip_first_line=true
+declare -i counter=0
 countries=()
+new_country_sql="INSERT INTO teams(name) VALUES"
+game_sql="INSERT INTO games(year, round, winner_id, opponent_id, winner_goals, opponent_goals) VALUES"
 echo $($PSQL "TRUNCATE TABLE games, teams")
 echo $($PSQL "ALTER SEQUENCE games_game_id_seq RESTART")
 echo $($PSQL "ALTER SEQUENCE teams_team_id_seq RESTART")
@@ -51,20 +54,29 @@ do
 
   if [  $pass_win -eq 0 ]
   then
-    new_country=$($PSQL "INSERT INTO teams(name) VALUES('$winner')")
+    new_country_sql+=" ('$winner'),"
     countries+=("$winner")
     winner_id=${#countries[@]}
-    echo -e "New country has been added: $winner, id = $winner_id"
+    echo -e "New country has been prepared to insert into database: $winner, id = $winner_id"
   fi
 
   if [ $pass_op -eq 0 ]
   then
-    new_country=$($PSQL "INSERT INTO teams(name) VALUES('$opponent')")
+    new_country_sql+=" ('$opponent'),"
     countries+=("$opponent")
     opponent_id=${#countries[@]}
-    echo -e "New country has been added: $opponent, id = $opponent_id"
+    echo -e "New country has been prepared to insert into database: $opponent, id = $opponent_id"
   fi
 
-  new_game=$($PSQL "INSERT INTO games(year, round, winner_id, opponent_id, winner_goals, opponent_goals) VALUES($year, '$round', $winner_id, $opponent_id, $winner_goals, $opponent_goals)")
-  echo -e "New game has been added: $winner vs $opponent"
+  game_sql+=" ($year, '$round', $winner_id, $opponent_id, $winner_goals, $opponent_goals),"
+  echo -e "New game has been prepared to insert into database: $winner vs $opponent"
 done < "games.csv"
+
+new_country_sql="${new_country_sql%,}"
+game_sql="${game_sql%,}"
+
+echo $new_country_sql
+echo $game_sql
+
+echo $($PSQL "$new_country_sql")
+echo $($PSQL "$game_sql")
