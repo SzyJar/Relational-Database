4df93568020a2d32983e0118dafa27d123588a33
SzyJar <107247457+SzyJar@users.noreply.github.com>
2023-05-20 03:06:06 +0200

    Salon appointment scheduler - done

--- Salon appointment scheduler/salon.sh
+++ Salon appointment scheduler/salon.sh
@@ -2,7 +2,7 @@
 
 PSQL="psql --username=freecodecamp --dbname=salon --tuples-only -c"
 
-echo -e "\n~~~~~ Salon Appointment Scheduler ~~~~~\n"
+echo -e "\n~~~~~ Salon Appointment Scheduler ~~~~~"
 
 SERVICES_MENU() {
   declare -i SERVICE_COUNT=0
@@ -15,14 +15,8 @@ SERVICES_MENU() {
     ((SERVICE_COUNT++))
   done < <(echo "$SERVICES")
 
-  echo -e "0) exit"
-
   read SERVICE_ID_SELECTED
-  if [[ $SERVICE_ID_SELECTED == 0 ]]
-  then
-    echo -e "\nThank you for stopping in.\n"
-    exit
-  elif [[ $SERVICE_ID_SELECTED =~ ^[0-9]+$ ]] && [[ $SERVICE_ID_SELECTED -le $SERVICE_COUNT ]]
+  if [[ $SERVICE_ID_SELECTED =~ ^[0-9]+$ ]] && [[ $SERVICE_ID_SELECTED -le $SERVICE_COUNT ]] && [[ $SERVICE_ID_SELECTED != 0 ]]
   then
     SERVICE $SERVICE_ID_SELECTED
   fi
@@ -32,23 +26,23 @@ SERVICES_MENU() {
 SERVICE() {
   SERVICE_NAME=$($PSQL "SELECT name FROM services WHERE service_id = $1")
   echo "What's your phone number?"
-  read PHONE_NUMBER
-  CUSTOMER_ID=$($PSQL "SELECT customer_id FROM customers WHERE phone = '$PHONE_NUMBER'")
+  read CUSTOMER_PHONE
+  CUSTOMER_ID=$($PSQL "SELECT customer_id FROM customers WHERE phone = '$CUSTOMER_PHONE'")
   if [[ -z $CUSTOMER_ID ]]
   then
     echo "I don't have a record for that phone number, what's your name?"
     read CUSTOMER_NAME
-    INSERT_CUSTOMER_INFO=$($PSQL "INSERT INTO customers(name, phone) VALUES('$CUSTOMER_NAME', '$PHONE_NUMBER')")
-    CUSTOMER_ID=$($PSQL "SELECT customer_id FROM customers WHERE phone = '$PHONE_NUMBER'")
+    INSERT_CUSTOMER_INFO=$($PSQL "INSERT INTO customers(name, phone) VALUES('$CUSTOMER_NAME', '$CUSTOMER_PHONE')")
+    CUSTOMER_ID=$($PSQL "SELECT customer_id FROM customers WHERE phone = '$CUSTOMER_PHONE'")
     else
-    CUSTOMER_NAME=$($PSQL "SELECT name FROM customers WHERE phone = '$PHONE_NUMBER'")
+    CUSTOMER_NAME=$($PSQL "SELECT name FROM customers WHERE phone = '$CUSTOMER_PHONE'")
   fi
-  echo "What time would you like your$SERVICE_NAME, $CUSTOMER_NAME."
+  echo "What time would you like your$SERVICE_NAME, $(echo $CUSTOMER_NAME | sed 's/ |/"/')."
   read SERVICE_TIME
   INSERT_APPOINTMENT=$($PSQL "INSERT INTO appointments(customer_id, service_id, time) VALUES('$CUSTOMER_ID', '$1', '$SERVICE_TIME')")
     
-  echo "I have put you down for a$SERVICE_NAME at $SERVICE_TIME, $CUSTOMER_NAME."
-  SERVICES_MENU "Welcome to My Salon, how can I help you?"
+  echo "I have put you down for a$SERVICE_NAME at $SERVICE_TIME, $(echo $CUSTOMER_NAME | sed 's/ |/"/')."
+  exit
 }
 
 SERVICES_MENU "Welcome to My Salon, how can I help you?"
